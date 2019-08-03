479d79cd2d2271c93bfbfcc3250e59e8aa1cf542
krishna <vnagarajan541@gmail.com>
2019-08-03 09:11:56 +0530

    My Project Setup Completed Successfully

---
+++ backend/src/main/java/com/krishna/backend/App.java
@@ -0,0 +1,13 @@
+package com.krishna.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
